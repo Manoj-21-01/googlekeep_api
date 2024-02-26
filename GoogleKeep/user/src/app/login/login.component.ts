18fc2403f22836917bbb18fdd5a8006b0f7ddda0
Manoj Veluru <manojveluru21@gmail.com>
2024-02-26 17:47:38 +0530

    [MANOJ] Added: added_loginapi

--- GoogleKeep/user/src/app/login/login.component.ts
+++ GoogleKeep/user/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-// import { UserService } from 'src/app/services/user services/user.service';
+import { UserService } from 'src/app/services/user-services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +15,7 @@ export class LoginComponent {
   ngOnInit(): void {
     
   }
-    constructor(private formBuilder: FormBuilder) {
+    constructor(private formBuilder: FormBuilder, public userService: UserService) {
       this.loginForm = this.formBuilder.group({
         email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
         password: ['', [Validators.required, Validators.minLength(6)]]
@@ -27,11 +27,11 @@ export class LoginComponent {
         }
     loginUser(){
       this.submitted = true;
-      // const {email, password} = this.loginForm.value;
-      // this.userService.loginUser({
-      //     "email": email,
-      //     "password": password
-      //   }).subscribe((result)=>{console.log(result);},error=>{console.log(error);});
+      const {email, password} = this.loginForm.value;
+      this.userService.loginUser({
+          "email": email,
+          "password": password
+        }).subscribe((result)=>{console.log(result);},error=>{console.log(error);});
       console.log(this.loginForm.value);
     }
 }
---
+++ GoogleKeep/user/src/app/services/user-services/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpService } from '../http-services/http.service';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  // use root for entire application and any for particular module
+  providedIn: 'root'
+})
+export class UserService {
+   // injecting dependency : accesstype 
+  constructor(public http:HttpService) { 
+
+
+  }
+  loginUser(data:object){
+    return this.http.loginApi(data);
+
+  }
+}
