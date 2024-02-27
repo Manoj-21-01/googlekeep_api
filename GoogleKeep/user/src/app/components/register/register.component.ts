a7c4772298f0010b6c5ec0bd1a0967bf7af1cd4f
Manoj Veluru <manojveluru21@gmail.com>
2024-02-27 16:56:04 +0530

    [MANOJ] Added: modified_register_code

---
+++ GoogleKeep/user/src/app/components/register/register.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserService } from 'src/app/services/user-services/user.service';
+
+
+@Component({ 
+  selector: 'app-register', 
+  templateUrl: 'register.component.html',
+  styleUrls: ['./register.component.css'],
+  host: {
+    class: "app-register-cnt"
+  }
+})
+export class RegisterComponent implements OnInit {
+  registerForm !: FormGroup
+  submitted: boolean = false
+  ngOnInit(): void {
+    
+  }
+    constructor(private formBuilder: FormBuilder, public userService: UserService) {
+      this.registerForm = this.formBuilder.group({
+        firstName: ['',[Validators.required, Validators.minLength(3)]],
+        lastName: ['',[Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+    }
+
+        get f() {
+          return this.registerForm.controls;
+        }
+        registerUser(){
+          this.submitted = true;
+          const {firstName, lastName, email, password} = this.registerForm.value;
+          this.userService.registerUser({
+            "firstName": firstName,
+            "lastName": lastName,
+            "email": email,
+            "password": password,
+            "service":"advance"
+          }).subscribe((result)=>{console.log(result);},error=>{console.log(error);});
+          console.log(this.registerForm.value);
+        }
+        
+}
